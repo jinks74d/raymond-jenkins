430a64f7740066d1b827dc0320b5c726a65acd80
Raymond Jenkins <raymond@farsidedev.com>
2024-12-03 13:13:11 -0600

    schema test

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Header from "../components/Header";
 import Footer from "@/components/Footer";
-import Head from 'next/head';
+import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +15,29 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Raymond Jenkins",
+    jobTitle: "Web Developer",
+    url: "https://raymondjenkins.com",
+    image: "https://raymondjenkins.com/images/raymond-jenkins.jpg",
+    sameAs: [
+      "https://www.linkedin.com/in/raymondjenkins1/",
+      "https://twitter.com/raymondjenkins",
+    ],
+  };
+
   return (
     <html lang="en" suppressHydrationWarning>
       <Head>
         <link rel="icon" href="/images/fwd-fav.png" />
       </Head>
+      <Script
+        id="json-ld-script"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <body
         className={`${inter.className} bg-light-bg dark:bg-dark-bg text-light-text dark:text-dark-text transition-colors duration-300`}
       >
